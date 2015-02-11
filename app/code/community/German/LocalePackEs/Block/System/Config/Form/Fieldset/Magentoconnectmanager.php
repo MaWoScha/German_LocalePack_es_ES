7cfd824d4ae2cfa7dc647ebdbf124595a85892ca
MaWoScha <manfredwormschaefer@gmail.com>
2015-02-10 22:44:19 -0500

    Version 0.2.0, 1. Translated Magento Connect Downloader, 2. Language packages (*.csv) of Magento Version 1.9.0.x.

---
+++ app/code/community/German/LocalePackEs/Block/System/Config/Form/Fieldset/Magentoconnectmanager.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @category  German
+ * @package   German_LocalePack
+ * @authors   MaWoScha <mawoscha@siempro.co, http://www.siempro.co/>
+ * @developer MaWoScha <mawoscha@siempro.co, http://www.siempro.co/>
+ * @version   0.2.0
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class German_LocalePackEs_Block_System_Config_Form_Fieldset_Magentoconnectmanager
+    extends Mage_Adminhtml_Block_System_Config_Form_Fieldset
+{
+    public function render(Varien_Data_Form_Element_Abstract $element)
+    {
+        $helper = Mage::helper("localepackes");
+
+        $html  = $this->_getHeaderHtml($element);
+        $html .= "<p style='font-weight:bold;'>".$helper->__("To install the translation for the Magento Connect Manager, proceed as follows:")."</p>";
+        $html .= "<ol style='list-style-position: outside; list-style-type: decimal; margin-left:18px;'>";
+        $html .= "<li>".$helper->__('In your Magento root directory in the subfolder "%s" rename the folder "%s" into "%s".',
+        		"/downloader",
+        		"template",
+        		"template_original" );
+        $html .= "</li>";
+        $html .= "<li>".$helper->__("The directory");
+        $html .= "<ul style='list-style-position: outside; list-style-type: disc; margin-left:18px;'>";
+        $html .= '<li>"template_es" '.$helper->__("or").'</li>';
+        $html .= '<li>"'.$helper->__("from").' Magento 1.5.x "template_es_1.5" '.$helper->__("or").'</li>';
+        $html .= '<li>"'.$helper->__("from").' Magento 1.7.x "template_es_1.7"</li></ul>';
+        $html .= $helper->__('rename into "template"').'.</li>';
+        $html .= "</ol>";
+        $html .= "<p>".$helper->__("The Magento Connect Manager should be changed to Spanish now.")."</p>";
+        $html .= "<p><strong style='font-weight: bold; color:red; margin-top:20pt;'>";
+        $html .= $helper->__("Please note:")."</strong> ".$helper->__('If Magento is updated, in particular the module "Mage_Downloader", the settings of the Connect Manager will be overwritten!')."<br />";
+        $html .= $helper->__('The same applies to the TinyMCE editor. Run in the case a "Reinstall" the language pack and repeat the described steps.')."</p>";
+        $html .= "</p>";
+        $html .= $this->_getFooterHtml($element);
+
+        return $html;
+    }
+}
