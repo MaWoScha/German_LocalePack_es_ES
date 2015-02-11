c2e24058760fed8a36265287db7eb948fc18f01e
MaWoScha <manfredwormschaefer@gmail.com>
2015-02-11 02:13:51 -0500

    Version 0.4.0, Translated E-Mail-Templates added (Magento Version 1.9.0.x) enhanced by three static blocks

---
+++ app/code/community/German/LocalePackEs/sql/localepackes_setup/mysql4-install-0.4.0.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @category  German
+ * @package   German_LocalePack
+ * @authors   MaWoScha <mawoscha@siempro.co, http://www.siempro.co/>
+ * @developer MaWoScha <mawoscha@siempro.co, http://www.siempro.co/>  
+ * @version   0.4.0.
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+$installer = $this;
+
+$installer->startSetup();
+
+$blockContent = <<<EOD
+Si tiene alguna duda respecto a su pedido, no deje de ponerse en contacto con nosotros
+{{depend store_email}}en email <a href=\"mailto:{{var store_email}}\">{{var store_email}}</a>,{{/depend}}
+{{depend store_phone}}en teléfono <a href=\"tel:{{var phone}}\">{{var store_phone}}</a>,{{/depend}}
+a través de <a title=\"Mi Servicio en Skype\" href=\"skype:mi.tienda?chat\" target=\"_blank\">Skype-Chat</a> (Nombre de usuario: mi.tienda)
+o en Facebook en nuestra <a title=\"Mi página de fans en Facebook\" href=\"http://www.facebook.com/mi.tienda\" target=\"_blank\">Mi página de fans</a>.
+EOD;
+
+$storeId = 2;
+
+$staticBlocks = array(
+    array(
+        'title'         => 'eMail Template Say Hello (es)',
+        'identifier'    => 'email_template_say_hello',
+        'content'       => 'Estimado/a',
+        'is_active'     => 0,
+        'stores'        => array($storeId)
+    ),
+    array(
+        'title'         => 'eMail Template Contact (es)',
+        'identifier'    => 'email_template_contact',
+        'content'       => $blockContent,
+        'is_active'     => 0,
+        'stores'        => array($storeId)
+    ),
+    array(
+        'title'         => 'eMail Template Say Bye (es)',
+        'identifier'    => 'email_template_say_bye',
+        'content'       => '¡Gracias por su confianza!',
+        'is_active'     => 0,
+        'stores'        => array($storeId)
+    )
+);
+
+/**
+ * Insert default blocks
+ */
+foreach ($staticBlocks as $data) {
+    try {
+        Mage::getModel('cms/block')->setData($data)->save();
+    } catch (Exception $e) {
+        # To prevent exception "A block identifier with the same properties already exists
+        # in the selected store." in "app:code:core:Mage:Cms:Model:Resource:Block.php"
+#        throw new Exception("Oops, mi error in ES German LocalePack");
+    }
+}
+
+$installer->endSetup();
+
+?>
